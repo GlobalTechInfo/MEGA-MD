8ec127721ab923e4e3b91cb6f7fc1570fd679c2e
GlobalTechInfo <raikasim1@gmqil.com>
2025-12-22 14:03:57 +0000

    Updated code to latest baileys 🌟

---
+++ plugins/ping.js
@@ -0,0 +1,56 @@
+/*****************************************************************************
+ *                                                                           *
+ *                     Developed By Qasim Ali                                *
+ *                                                                           *
+ *  🌐  GitHub   : https://github.com/GlobalTechInfo                         *
+ *  ▶️  YouTube  : https://youtube.com/@GlobalTechInfo                       *
+ *  💬  WhatsApp : https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07     *
+ *                                                                           *
+ *    © 2026 GlobalTechInfo. All rights reserved.                            *
+ *                                                                           *
+ *    Description: This file is part of the MEGA-MD Project.                 *
+ *                 Unauthorized copying or distribution is prohibited.       *
+ *                                                                           *
+ *****************************************************************************/
+ 
+
+module.exports = {
+  command: 'ping',
+  aliases: ['p', 'pong'],
+  category: 'general',
+  description: 'Check bot response time',
+  usage: '.ping',
+  
+  async handler(sock, message, args) {
+    const start = Date.now();
+    const chatId = message.key.remoteJid;
+    
+    const sent = await sock.sendMessage(chatId, { 
+      text: 'Pinging...' 
+    });
+    
+    const end = Date.now();
+    
+    await sock.sendMessage(chatId, {
+      text: `🏓 Pong!\nLatency: ${end - start}ms`,
+      edit: sent.key
+    });
+  }
+};
+
+
+/*****************************************************************************
+ *                                                                           *
+ *                     Developed By Qasim Ali                                *
+ *                                                                           *
+ *  🌐  GitHub   : https://github.com/GlobalTechInfo                         *
+ *  ▶️  YouTube  : https://youtube.com/@GlobalTechInfo                       *
+ *  💬  WhatsApp : https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07     *
+ *                                                                           *
+ *    © 2026 GlobalTechInfo. All rights reserved.                            *
+ *                                                                           *
+ *    Description: This file is part of the MEGA-MD Project.                 *
+ *                 Unauthorized copying or distribution is prohibited.       *
+ *                                                                           *
+ *****************************************************************************/
+ 
